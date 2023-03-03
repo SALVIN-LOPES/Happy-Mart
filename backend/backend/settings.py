6521a6809e19714715e85ce5b1f48e35f25d3a3c
Salvin Lopes <salvinlopes20@gmail.com>
2023-03-03 17:00:56 +0530

    postgres DB added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,17 +1,36 @@
 import os
+
+# db connection 
+import json
+from django.core.exceptions import ImproperlyConfigured
+
 from pathlib import Path
 from datetime import timedelta
-from security import *
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+# db secure connection method : 
+with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+    secrets = json.load(secrets_file)
+
+def get_secret(setting, secrets=secrets):
+    """Get secret setting or fail with ImproperlyConfigured"""
+    try:
+        return secrets[setting]
+    except KeyError:
+        raise ImproperlyConfigured("Set the {} setting".format(setting))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7mq_rl*r4%r(ua#yyqd&4%^6g@17=4n8dtgpr_@b_xl8p)f!7%'
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,23 +133,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# old database : sqlite database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # new database : postgres database
 DATABASES={
    'default':{
-      'ENGINE':DB_ENGINE,
-      'NAME':DB_NAME,
-      'USER':DB_USER,
-      'PASSWORD':DB_PASSWORD,
-      'HOST':DB_HOST,
-      'PORT':DB_PORT,
+      'ENGINE':get_secret('DB_ENGINE'),
+      'NAME':get_secret('DB_NAME'),
+      'USER':get_secret('DB_USER'),
+      'PASSWORD':get_secret('DB_PASSWORD'),
+      'HOST':get_secret('DB_HOST'),
+      'PORT':get_secret('DB_PORT'),
    }
 }
 
